f6af2073871130d468c32f1c2f9f7a954d1b5c64
afridi <developermac07@gmial.com>
2024-08-07 21:25:24 +0530

    first commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,6 +15,7 @@ public class App
     public static void main( String[] args )
     {
 //        System.out.println( "Hello World!" );
+//        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
         ApplicationContext context =new ClassPathXmlApplicationContext("confige.xml");
 //            Student student1 =(Student) context.getBean("student1");git commit -m "Reset repository"App
             Student student2 =(Student) context.getBean("student2");
